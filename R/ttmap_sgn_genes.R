cf4491e8d21109680b09238647e48ce1e3fc01df
chronchi <carloshvronchi@gmail.com>
2020-04-23 13:31:36 +0200

    remove man and vignettes

--- R/ttmap_sgn_genes.R
+++ R/ttmap_sgn_genes.R
@@ -1,3 +1,41 @@
+# extract pvalues from the dataframe. the default key is p.value
+extract_pvalues <- function(dataframe){
+  # extract keys
+  df_names <- names(dataframe)
+  # create empty array
+  pvalues <- c()
+  tvalues <- c()
+  # iterate over keys to extract pvalues
+  for (n in df_names){
+    pvalues <- c(pvalues, dataframe[[n]]$p.value)
+    tvalues <- c(tvalues, dataframe[[n]]$statistic)
+  }
+  stats_ <- list(pvalues, tvalues)
+  return(stats_)
+}
+
+# create dataframe containing p.values mean deviation component, 
+# adjusted p-values and gene names. The input is a table wit t.test results
+create_dataframe <- function(ttest_results){
+  # first extract p-values
+  stats_ <- extract_pvalues(ttest_results)
+  # adjust pvalues
+  adjus_pvalues <- p.adjust(stats_[[1]], method='BH')
+  # create final dataframe with gene name, deviation component, p-value and 
+  # adjusted p-value
+  genes <- names(ttest_results)
+  mean_dev_com <- c()
+  for(n in genes){
+    mean_dev_com <- c(mean_dev_com, as.numeric(ttest_results[[n]]$estimate))
+  }
+  final_dataframe = data.frame(Gene=genes, 
+                               logFC = mean_dev_com,
+                               t = stats_[[2]],
+                               P.Value = stats_[[1]],
+                               adj.P.Val = adjus_pvalues)
+  return(final_dataframe)
+}
+
 ttmap_sgn_genes <- function(ttmap_part2_gtlmap, 
                             ttmap_part1_hda, 
                             ttmap_part1_ctrl_adj, 
@@ -46,6 +84,20 @@ ttmap_sgn_genes <- function(ttmap_part2_gtlmap,
             }
 
             # A <- cbind(annot[rownames(A), col], A)
+            
+            # calculate the p-values associated for each gene. 
+            # the null hypothesis is that the genes don't deviate 
+            # from the control group, i.e., their deviation components
+            # are zero. 
+            
+            #t_tests <- apply(as.matrix(A), 1, t.test)  
+            
+            #t_results <- create_dataframe(t_tests)
+
+            # append the p value and adjusted p value columns to the original
+            # dataframe
+            #A <- cbind('adj_p_value'=t_results$adj.P.Val, A)
+            #A <- cbind('p-value'=t_results$P.Value, A)
             
             tier_strip = str_remove(tier, '_.*')
             
